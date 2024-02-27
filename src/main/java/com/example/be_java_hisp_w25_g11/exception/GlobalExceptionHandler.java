059d9e838f309ca6f210a5bee8d4bb942b1c1505
jcalderondom <juancamilo.calderondominguez@mercadolibre.com.co>
2024-02-27 11:04:55 -0500

    Chore/settings pre sprint2 (#1)
    * Fix Types
    
    grupal
    
    * Add dependencies
    
    * Add validations and exception handlers
    
    Add validations
    Exception handlers
    Custom message
    Test files

--- src/main/java/com/example/be_java_hisp_w25_g11/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/be_java_hisp_w25_g11/exception/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.example.be_java_hisp_w25_g11.exception;
 
 import com.example.be_java_hisp_w25_g11.dto.ExceptionDTO;
+import com.example.be_java_hisp_w25_g11.dto.ExceptionListDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,5 +33,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> alreadyExists(AlreadyExistsException e){
         ExceptionDTO exceptionDTO = new ExceptionDTO(e.getMessage());
         return new ResponseEntity<>(exceptionDTO, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> argumentNotValid(MethodArgumentNotValidException e){
+        //ExceptionDTO exceptionDTO = new ExceptionDTO(e.getBindingResult().getFieldError().getDefaultMessage());
+        ExceptionListDTO exceptionDTO = new ExceptionListDTO();
+
+        exceptionDTO.setMessages(e.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> fieldError.getDefaultMessage())
+                .collect(Collectors.toList()));
+
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/test/java/com/example/be_java_hisp_w25_g11/repository/seller_post/SellerPostRepositoryImpTest.java
@@ -0,0 +1,36 @@
+package com.example.be_java_hisp_w25_g11.repository.seller_post;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SellerPostRepositoryImpTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void createAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void existing() {
+    }
+}
