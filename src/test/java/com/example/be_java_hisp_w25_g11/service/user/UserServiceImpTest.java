5b4fc101983c6e949d4aecdc7d1e928a07c23f6e
David Mateo García Vallejo <davidmgarciav@gmail.com>
2024-02-27 16:40:06 -0500

    Create sortFollowed existence order tests
    Create two tests for sortFollowed method of service UserService, testing the existence or not of the input order.

--- src/test/java/com/example/be_java_hisp_w25_g11/service/user/UserServiceImpTest.java
+++ src/test/java/com/example/be_java_hisp_w25_g11/service/user/UserServiceImpTest.java
@@ -54,6 +54,8 @@ class UserServiceImpTest {
     void sortFollowers() {
     }
 
+    // T-0003: Verificar que el tipo de ordenamiento alfabético exista para los seguidores
+    // Resultado: Permite continuar con normalidad.
     @Test
     void testSortFollowersValidOrder() {
         // Arrange
@@ -71,6 +73,8 @@ class UserServiceImpTest {
         Assertions.assertDoesNotThrow(() -> userService.sortFollowers(sellerId, noOrder));
     }
 
+    // T-0003: Verificar que el tipo de ordenamiento alfabético no exista para los seguidores
+    // Resultado: Notifica la no existencia mediante una excepción.
     @Test
     void testSortFollowersInvalidOrder() {
         // Arrange
@@ -91,12 +95,40 @@ class UserServiceImpTest {
     void sortFollowed() {
     }
 
+    // T-0003: Verificar que el tipo de ordenamiento alfabético exista para los seguidos
+    // Resultado: Permite continuar con normalidad.
     @Test
-    void sortFollowedTestOrderExists() {
+    void testSortFollowedValidOrder() {
+        // Arrange
+        Integer buyerId = 1;
+        Buyer buyer = new Buyer(buyerId, "buyer");
+        String orderAsc = "name_asc";
+        String orderDesc = "name_desc";
+        String noOrder = null;
+        when(buyerRepository.get(buyerId)).thenReturn(Optional.of(buyer));
+        when(sellerRepository.get(buyerId)).thenReturn(Optional.empty());
+        // Act & Assert
+        Assertions.assertDoesNotThrow(() -> userService.sortFollowed(buyerId, orderAsc));
+        Assertions.assertDoesNotThrow(() -> userService.sortFollowed(buyerId, orderDesc));
+        Assertions.assertDoesNotThrow(() -> userService.sortFollowed(buyerId, noOrder));
     }
 
+    // T-0003: Verificar que el tipo de ordenamiento alfabético no exista para los seguidos
+    // Resultado: Notifica la no existencia mediante una excepción.
     @Test
-    void sortFollowedTestOrderNoExists() {
+    void testSortFollowedInvalidOrder() {
+        // Arrange
+        Integer buyerId = 1;
+        Buyer buyer = new Buyer(buyerId, "buyer");
+        String failOrderAsc = "asc";
+        String failOrderDesc = "desc";
+        String otherOrder = "empanada";
+        when(buyerRepository.get(buyerId)).thenReturn(Optional.of(buyer));
+        when(sellerRepository.get(buyerId)).thenReturn(Optional.empty());
+        // Act & Assert
+        Assertions.assertThrows(BadRequestException.class, () -> userService.sortFollowed(buyerId, failOrderAsc));
+        Assertions.assertThrows(BadRequestException.class, () -> userService.sortFollowed(buyerId, failOrderDesc));
+        Assertions.assertThrows(BadRequestException.class, () -> userService.sortFollowed(buyerId, otherOrder));
     }
 
     @Test
